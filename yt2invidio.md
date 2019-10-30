44332eb889d35809d4f2827319966cf04c86eeb0
Izzy <izzy@qumran.org>
2019-10-30 22:55:34 +0100

    YT2Invidio: rewrite, now Invidious instance can be set via GM MenuCommand

--- yt2invidio.md
+++ yt2invidio.md
@@ -8,9 +8,10 @@ If you are using the [Mouseover Popup Image Viewer](https://greasyfork.org/de/sc
 Additionally, since version 1.1.0 (2019-09-05) also rewrites Twitter links to Nitter.Net. Reasoning is the same – and Nitter is to Twitter what Invidio.us is to YouTube.
 
 ### How to select your preferred instance?
-If you don't like the pre-configured one, you can simply change the `videohost` variable at the top of the script. Note that by editing the script inside Greasemonkey, the monkey will stop updating to preserve your changes – so you will then need to update the script manually.
+If you don't like the pre-configured one, you can simply change it via the UserScript menu. A list of instances can be found [here](https://github.com/omarroth/invidious/wiki/Invidious-Instances).
 
 ### Script history:
+* v1.2.0 (2019-10-30): rewrite. Now you can set the Invidious instance to be used via menu command, no script edit needed.
 * v1.1.2 (2019-10-07): make it easier to switch to your favorite Invidious instance (just change a single variable); make invidious.snopyta.org the default
 * v1.1.1 (2019-09-06): do not exclude invidio.us but just ignore YT links there instead
 * v1.1.0 (2019-09-05): add Twitter2Nitter
--- yt2invidio.user.js
+++ yt2invidio.user.js
@@ -1,34 +1,58 @@
 // ==UserScript==
 // @name        YT2Invidio
-// @namespace   http://projects.izzysoft.de/
-// @author      IzzySoft
+// @namespace   de.izzysoft
+// @author      Izzy
 // @description Point YouTube links to Invidio -- and Twitter links to Nitter
 // @license     CC BY-NC-SA
 // @include     *
-// @version     1.1.2
+// @version     1.2.0
 // @run-at      document-idle
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
 // @grant       unsafeWindow
 // @homepageURL https://codeberg.org/izzy/userscripts
 // @downloadURL https://codeberg.org/izzy/userscripts/raw/branch/master/yt2invidio.user.js
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // ==/UserScript==
 
-// Define your preferred instance here.
-// A list of available instances can be found at
-// https://github.com/omarroth/invidious/wiki/Invidious-Instances
-videohost='invidious.snopyta.org';
+// Get the Invidious instance to use for rewrite
+GM.getValue('InvidiousHost','invidiou.sh').then(function(result) {
+  rewriteLinks(result);
+});
 
-for(var i = 0; i < document.links.length; i++) {
-  var elem = document.links[i];
 
-  // Youtube: https://www.youtube.com/watch?v=cRRA2xRRgl8 || https://www.youtube.com/channel/dfqwfhqQ34er
-  // only rewrite if we're not on Invidious already (too keep the "watch this on YouTube" links intact)
-  if (elem.href.match(/(www\.)?youtube.com(\/watch\?v=[a-z0-9_-]+)/i) || elem.href.match(/(www\.)?youtube.com(\/channel\/[a-z0-9_-]+)/i)) {
-    if (location.hostname != videohost) { elem.href='https://'+videohost+RegExp.$2; }
-  } else if (elem.href.match(/(www\.)?youtu.be\/([a-z0-9_-]+)/i)) {
-    if (location.hostname != videohost) { elem.href='https://'+videohost+'/watch?v='+RegExp.$2; }
+// Do the actual rewrite
+function rewriteLinks(videohost) {
+  console.log(`Using '${videohost}' for rewrite`);
 
-  // Twitter
-  } else if (elem.href.match(/(mobile\.)?twitter\.com\/([^&#]+)/i)) {
-    elem.href='https://nitter.net/'+RegExp.$2;
+  for(var i = 0; i < document.links.length; i++) {
+    var elem = document.links[i];
+
+    // Youtube: https://www.youtube.com/watch?v=cRRA2xRRgl8 || https://www.youtube.com/channel/dfqwfhqQ34er
+    // only rewrite if we're not on Invidious already (too keep the "watch this on YouTube" links intact)
+    if (elem.href.match(/(www\.)?youtube.com(\/watch\?v=[a-z0-9_-]+)/i) || elem.href.match(/(www\.)?youtube.com(\/channel\/[a-z0-9_-]+)/i)) {
+      if (location.hostname != videohost) { elem.href='https://'+videohost+RegExp.$2; }
+    } else if (elem.href.match(/(www\.)?youtu.be\/([a-z0-9_-]+)/i)) {
+      if (location.hostname != videohost) { elem.href='https://'+videohost+'/watch?v='+RegExp.$2; }
+
+    // Twitter
+    } else if (elem.href.match(/(mobile\.)?twitter\.com\/([^&#]+)/i)) {
+      elem.href='https://nitter.net/'+RegExp.$2;
+    }
   }
 }
+
+
+// Give the user the possibility to set a different preferred instance
+// A list of available instances can be found at
+// https://github.com/omarroth/invidious/wiki/Invidious-Instances
+async function setInstance() {
+  let vhost = await GM.getValue('InvidiousHost','invidiou.sh');
+  vhost = prompt('Set Invidious instance to:', vhost);
+  if (vhost != null) GM.setValue('InvidiousHost',vhost);
+}
+
+GM_registerMenuCommand('Set Invidious instance',setInstance);
